2bcde71234a52e3ff3dbcfdafb39a28e7a40884d
Drew <9892074+andmpel@users.noreply.github.com>
2025-05-16 16:15:04 -0500

    Bug in previous commit
    copy paste format issue

--- update-all.sh
+++ update-all.sh
@@ -153,24 +153,23 @@ check_internet() {
     return 0
 }
 
-
 update_all() {
-  if ! check_internet; then
-    exit 1
-  }
+    # Check if internet is available
+    if ! check_internet; then
+        exit 1
+    fi
 
-  update_brew &
-  update_vscode &
-  update_gem &
-  update_npm &
-  update_yarn &
-  update_cargo &
-  update_app_store &
-  update_macos &
+    update_brew &
+    update_vscode &
+    update_gem &
+    update_npm &
+    update_yarn &
+    update_cargo &
+    update_app_store &
+    update_macos &
 
-  wait
+    wait
 }
-
 
 # COMMENT OUT IF SOURCING
 update_all
