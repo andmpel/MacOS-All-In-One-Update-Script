32526051317520c515719ff728ac0f58d9fa97ef
Drew <9892074+andmpel@users.noreply.github.com>
2025-08-24 18:37:01 -0500

    Add --yes flag to remove prompt for user selection and to use the previous selection upon exection.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"macup/macup"
 	"os"
@@ -9,6 +10,10 @@ import (
 
 // Entry point of the update script
 func main() {
+	// Define and parse the --yes flag
+	yes := flag.Bool("yes", false, "Use previous selections without prompting")
+	flag.Parse()
+
 	// Check for internet connectivity before proceeding
 	if !macup.CheckInternet() {
 		os.Exit(1)
@@ -22,17 +27,26 @@ func main() {
 	}
 
 	// Prompt the user to select updates
-	selectedUpdates, err := macup.SelectUpdates(config)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error selecting updates: %v\n", err)
-		os.Exit(1)
-	}
+	var selectedUpdates []string
+	if *yes && len(config.SelectedUpdates) > 0 {
+		selectedUpdates = config.SelectedUpdates
+	} else {
+		if *yes {
+			println("No previous updates selected. Prompting for selection.")
+		}
+		var err error
+		selectedUpdates, err = macup.SelectUpdates(config)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error selecting updates: %v\n", err)
+			os.Exit(1)
+		}
 
-	// Save the user's selections
-	config.SelectedUpdates = selectedUpdates
-	if err := config.SaveConfig(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error saving config: %v\n", err)
-		os.Exit(1)
+		// Save the user's selections
+		config.SelectedUpdates = selectedUpdates
+		if err := config.SaveConfig(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error saving config: %v\n", err)
+			os.Exit(1)
+		}
 	}
 
 	// Run the selected updates
